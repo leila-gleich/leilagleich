fca82c15143337af17a3619b61e078109153c9cf
Leila Glei <leilair@Leilas-MBP.home>
2017-09-28 12:27:01 -0400

    Add case studies controller and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'case_studies/index', to: "case_studies#index", as: "case-studies"
+
+  get 'case_studies/airline_industry', to: "case_studies#airline_industry", as: "airline-industry"
+
   get 'position/index', to: "position#index", as: "professional-experience"
 
   get 'position/create'
